5c68b6a4cab160b05c40765a60980162934831ca
CodingCyrus <cyrushc@bu.edu>
2022-10-17 06:45:20 -0400

    Initial Commit

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '052d883963msh693e4164e30db95p13756bjsnde8cfa735493',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
